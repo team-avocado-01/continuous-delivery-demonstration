5681147c07a6b04d74e7e7a5ef7be235714858d0
Matt Coleman <matt.coleman@sainsburys.co.uk>
2019-07-10 09:15:39 +0100

    Add bugfixes; comment functions

--- test_transform.py
+++ test_transform.py
@@ -3,3 +3,7 @@ from transform import *
 
 def test_doNothing():
     assert doNothing() == 'Nothing to see here'
+
+
+def test_num_std_devs():
+    assert num_std_devs > 0
--- transform.py
+++ transform.py
@@ -2,18 +2,27 @@ import pandas
 import sys
 
 
+# Filepaths
 input_fp = './input/titanic.csv'
 output_fp = './output/outliers.csv'
 
 
+# Input parameters
+num_std_devs = 2        # Number of standard deviations from the mean
+col_analyse = 'Age'     # Which column are we checking?
+
+
+# Does nothing
 def doNothing():
     return 'Nothing to see here'
 
 
+# Reads input csv data
 def readData(filepath):
     return pandas.read_csv(filepath)
 
 
+# TODO
 def getOutliers(df, n_std_dev, col):
     mean = df[col].mean()
     print(mean)
@@ -25,13 +34,15 @@ def getOutliers(df, n_std_dev, col):
     return pandas.concat([df_max, df_min])
 
 
+# Saves the output to disk
 def storeOutliers(df, filepath):
     df.to_csv(filepath)
 
 
+# Main method - testing
 if __name__ == '__main__':
     print('testing')
     df = readData(input_fp)
-    df = getOutliers(df, float(sys.argv[1]), str(sys.argv[2]))
+    df = getOutliers(df, float(num_std_devs), str(col_analyse))
     storeOutliers(df, output_fp)
     print(df)
