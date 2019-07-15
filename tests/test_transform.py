2f039de53ecea8ee17cd9cbffc62f04d670de407
Matt Coleman <matt.coleman@sainsburys.co.uk>
2019-07-15 09:25:11 +0100

    Add pytest for getOutliers()

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,9 +1,18 @@
 from src.transform import *
+import pandas as pd
 
 
 # Dummy test; should pass regardless of how good our code is
 def test_doNothing():
     assert doNothing() == 'Nothing to see here'
+
+
+def test_getOutliers():
+    data1 = {'col1': [1, 1, 1, 1, 1, 99999999],
+             'col2': [1, 1, 1, 1, 1, 1],
+             'col3': [1, 1, 1, 1, 1, 1]}
+    df1 = pd.DataFrame(data=data1)
+    assert getOutliers(df1, 1, col1) == df1.iloc[6]
 
 
 def test_num_std_devs():
