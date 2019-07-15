9d64837d7a7e1a17fbd011efd0ff7a2f6e4dcaf4
Matt Coleman <matt.coleman@sainsburys.co.uk>
2019-07-15 12:43:29 +0100

    Add skeleton steps.py for behave testing

--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -1,2 +1,32 @@
 from behave import *
 from algebra import *
+
+
+@given('a specific set of titanic passengers')
+def step_impl():
+    pass
+
+
+@when('we list only the survivors name, sex and age')
+def step_impl():
+    pass
+
+
+@then('we observe')
+def step_impl():
+    pass
+
+
+@given('a specific set of titanic passengers')
+def step_impl():
+    pass
+
+
+@when('we search for those with statistically extraordinary age')
+def step_impl():
+    pass
+
+
+@then('we observe')
+def step_impl():
+    pass
