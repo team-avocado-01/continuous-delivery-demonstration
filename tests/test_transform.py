0e439ac925286e47126bfa6134d8ea88fe4878da
Matt Coleman <matt.coleman@sainsburys.co.uk>
2019-07-16 15:40:38 +0100

    Comment out previous tests; begin to write new ones via 3A/decorators

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -2,26 +2,34 @@ from src.transform import *
 import pandas as pd
 
 
-# Dummy test; should pass regardless of how good our code is
-def test_doNothing():
-    assert doNothing() == 'Nothing to see here'
+# # Dummy test; should pass regardless of how good our code is
+# def test_doNothing():
+#     assert doNothing() == 'Nothing to see here'
 
 
-# Tests whether the getOutliers function correctly does so
+# # Tests whether the getOutliers function correctly does so
+# def test_getOutliers():
+#     data1 = {'col1': [1, 1, 1, 1, 1, 99999999],
+#              'col2': [1, 1, 1, 1, 1, 1],
+#              'col3': [2, 2, 1.9, 2, 2, 2]}
+#     df1 = pd.DataFrame(data=data1)
+#     # Test an obvious outlier:
+#     assert getOutliers(df1, 1, 'col1').equals(df1[df1['col1'] == 99999999])
+#     # Test when there's no outliers:
+#     assert getOutliers(df1, 1, 'col2').empty
+#     assert getOutliers(df1, 999, 'col1').empty
+#     # Test checking for tiny variations; sigma very small:
+#     assert getOutliers(df1, 0.001, 'col2').empty
+
+
+# # Ensures we are checking for outliers a valid number of standard deviations from the mean
+# def test_num_std_devs():
+#     assert num_std_devs > 0
+
+# @pytest.fixture
 def test_getOutliers():
-    data1 = {'col1': [1, 1, 1, 1, 1, 99999999],
-             'col2': [1, 1, 1, 1, 1, 1],
-             'col3': [2, 2, 1.9, 2, 2, 2]}
-    df1 = pd.DataFrame(data=data1)
-    # Test an obvious outlier:
-    assert getOutliers(df1, 1, 'col1').equals(df1[df1['col1'] == 99999999])
-    # Test when there's no outliers:
-    assert getOutliers(df1, 1, 'col2').empty
-    assert getOutliers(df1, 999, 'col1').empty
-    # Test checking for tiny variations; sigma very small:
-    assert getOutliers(df1, 0.001, 'col2').empty
-
-
-# Ensures we are checking for outliers a valid number of standard deviations from the mean
-def test_num_std_devs():
-    assert num_std_devs > 0
+    """
+    getOutliers() extracts a subset of a DataFrame containing elements which are a specified no.
+    of standard deviations from the mean.
+    """
+    # Arrange, Act, Assert...
