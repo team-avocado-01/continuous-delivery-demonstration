6f83edaa38ffe2a2b31ac62fadd66e0bbb66002c
Matt Coleman <matt.coleman@sainsburys.co.uk>
2019-07-18 09:41:59 +0100

    Write unit tests for outlier detection in 3A format, using fixtures

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,51 +1,25 @@
 from src.transform import *
 import pandas as pd
+import numpy as np
 
 
-# # Dummy test; should pass regardless of how good our code is
-# def test_doNothing():
-#     assert doNothing() == 'Nothing to see here'
-
-
-# # Tests whether the getOutliers function correctly does so
-# def test_getOutliers():
-#     data1 = {'col1': [1, 1, 1, 1, 1, 99999999],
-#              'col2': [1, 1, 1, 1, 1, 1],
-#              'col3': [2, 2, 1.9, 2, 2, 2]}
-#     df1 = pd.DataFrame(data=data1)
-#     # Test an obvious outlier:
-#     assert getOutliers(df1, 1, 'col1').equals(df1[df1['col1'] == 99999999])
-#     # Test when there's no outliers:
-#     assert getOutliers(df1, 1, 'col2').empty
-#     assert getOutliers(df1, 999, 'col1').empty
-#     # Test checking for tiny variations; sigma very small:
-#     assert getOutliers(df1, 0.001, 'col2').empty
-
-
-# # Ensures we are checking for outliers a valid number of standard deviations from the mean
-# def test_num_std_devs():
-#     assert num_std_devs > 0
-
-# exampleData is a fixture in conftest.py
-def test_getOutliers(exampleData):
+def test_getOutliers(titanicKnownOutliers):
     """
-    getOutliers() extracts a subset of a DataFrame containing elements which are a specified no.
-    of standard deviations from the mean.
+    getOutliers() produces a subset of a DataFrame containing elements which are a specified no.
+    of standard deviations from the mean or more. We compare computed outliers to known-correct
+    outliers for a range of inputs, for our sample dataset.
     """
-    # Arrange (Our data is already in the necessary form...)
-    categories = exampleData.columns
-    std_dev_range = range(-5, 6)
-    outputs = []
-    expected_outputs = []
+    # Arrange
+    std_dev = titanicKnownOutliers['input'][0]
+    cat_col = titanicKnownOutliers['input'][1]
+    d_frame = titanicKnownOutliers['data']
 
     # Act
-    for cat in categories:
-        for std_devs in std_dev_range:
-            outputs.append(getOutliers(exampleData, std_devs, cat))
+    computed = getOutliers(d_frame, std_dev, cat_col).sort_values(by=['PassengerId'])
+    expected = d_frame[d_frame['PassengerId'].isin(titanicKnownOutliers['expected'])].sort_values(by=['PassengerId'])
+    pandas.set_option('display.max_columns', None)
+    print(computed)
+    print(expected)
 
     # Assert
-    assert outputs == expected_outputs
-
-
-if __name__ == '__main__':
-    print(test_getOutliers(exampleData))
+    assert computed.equals(expected)
