b142f853a81659940d1aa680769c6d054359580d
Matt Coleman <matt.coleman@sainsburys.co.uk>
2019-07-19 09:51:55 +0100

    Remove extraneous code; leave only new test structure

--- conftest.py
+++ conftest.py
@@ -4,51 +4,13 @@ import numpy as np
 from src.transform import *
 
 
-"""
-Define test cases here. Each dict has two key/value pairs:
-    {"input": (Number_of_std_devs, Column_to_check),
-    "output": [PassengerIDs, of, outlying, passengers]}
-"""
-
-
 @pytest.fixture()
 def titanicSamples(request):
     """
-    Setup code;
+    Simple fixture;
     Yields a sample dataset detailing survivors/unfortunates on RMS Titanic
     """
     fp = './sample-data/input/titanic.csv'
     print('Running setup code (if there actually was any...)')
     yield pd.read_csv(fp)
-    print('Running teardown code (if there actually was any...)')
-
-
-@pytest.fixture(params=[{"input": (0, "Age"),
-                         "expected": list(range(1, 10))},
-                        {"input": (1, "Age"),
-                         "expected": [6, 7, 8]},
-                        {"input": (2, "Age"),
-                         "expected": [6]},
-                        {"input": (0, "Fare"),
-                         "expected": list(range(1, 10))},
-                        {"input": (1, "Fare"),
-                         "expected": [2, 4, 7]},
-                        {"input": (999, "Fare"),
-                         "expected": []},
-                        {"input": (1.23, "Age"),
-                         "expected": [6, 7, 8]},
-                        {"input": (0.123, "Fare"),
-                         "expected": list(range(1, 10))}])
-def titanicKnownOutliers(request):
-    """
-    Setup code;
-    Yields a set of known outliers and the corresponding parameters that are
-    expected to produce them from our titanic dataset.
-
-    Inputs are tuples of (n_std_dev, column_name)
-    """
-    fp = './sample-data/input/titanic.csv'
-    print('Running setup code (if there actually was any...)')
-    request.param['data'] = pd.read_csv(fp)
-    yield request.param
     print('Running teardown code (if there actually was any...)')
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -3,12 +3,23 @@ import pandas as pd
 import numpy as np
 import pytest
 
-@pytest.mark.parametrize("input,expected", [((0, "Age"), list(range(1, 10))), ((1, "Age"), [6, 7, 8]), ((2, "Age"), [6]), ((0, "Fare"), list(range(1, 10))), ((1, "Fare"), [2, 4, 7]), ((999, "Fare"), []), ((1.23, "Age"), [6, 7, 8]), ((0.123, "Fare"), list(range(1, 10)))])
+
+@pytest.mark.parametrize("input,expected", [((0, "Age"), list(range(1, 10))),
+                                            ((1, "Age"), [6, 7, 8]),
+                                            ((2, "Age"), [6]),
+                                            ((0, "Fare"), list(range(1, 10))),
+                                            ((1, "Fare"), [2, 4, 7]),
+                                            ((999, "Fare"), []),
+                                            ((1.23, "Age"), [6, 7, 8]),
+                                            ((0.123, "Fare"), list(range(1, 10)))])
 def test_getOutliers(input, expected, titanicSamples):
     """
-    getOutliers() produces a subset of a DataFrame containing elements which are a specified no.
-    of standard deviations from the mean or more. We compare computed outliers to known-correct
-    outliers for a range of inputs, for our sample dataset.
+    getOutliers() produces a subset of a DataFrame containing elements which are at least
+    a specified no. of standard deviations from the mean. We compare computed outliers to
+    known-correct outliers for a range of inputs, for our sample dataset.
+
+    input, tuple: (num_std_devs, column_to_check)
+    expected, list: [list_of, passenger_ids, present]
     """
     # Arrange
     std_dev = input[0]
